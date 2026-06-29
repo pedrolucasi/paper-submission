ef32ffd6fab44c8d121ac1ed1b982d634f8ebbb1
pedrolucasi <pp.pedro.sb@gmail.com>
2026-06-28 21:26:59 -0300

    chore(config/deps): estabelece setup inicial do projeto Maven para SciReview

---
+++ src/main/java/br/edu/ifpb/cstsi/pss/scireview/App.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.cstsi.pss.scireview;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("SciReview - Sistema de Submissão de Artigos");
+    }
+}
---
+++ src/test/java/br/edu/ifpb/cstsi/pss/scireview/AppTest.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.cstsi.pss.scireview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
